d6b27d3b141bdf5adcaaa6513c17afc34ce218d1
samanta-amit <amit.samanta049@gmail.com>
2019-04-09 13:58:13 +0200

    trying to create multiple workers

--- simulator.py
+++ simulator.py
@@ -3,7 +3,8 @@ from collections import deque
 from functools import partial
 import heapq
 from enum import Enum
-
+import random
+from random import randint
 
 class EventQueue:
 
@@ -12,12 +13,13 @@ class EventQueue:
 		self.i = 0
 		self.t = 0.0
 
-	def schedule(self, delta_t, callback):
-		heapq.heappush(self.events, (self.t + delta_t, self.i, callback))
+	def schedule(self, delta_t, priority, callback):
+		delta_t += random.randint(1,8)*0.02 
+		heapq.heappush(self.events, (self.t + delta_t, priority, self.i, callback))
 		self.i += 1
 
 	def advance(self):
-		t, i, callback = heapq.heappop(self.events)
+		t, priority, i, callback = heapq.heappop(self.events)
 		self.t = t
 		return callback
 
@@ -35,29 +37,35 @@ def t_str(t):
 	micros = int(t * 1000000) % 1000
 	return "%d.%03d,%03d" % (seconds, millis, micros)
 
-# def print(s):
-# 	__builtins__.print("%s - %s" % (t_str(now()).rjust(11), s))
+def print_m(s):
+	__builtins__.print_m("%s - %s" % (t_str(now()).rjust(11), s))
 
-def logEvent(event_type, message):
-	print("%s %s" % (event_type.ljust(15), message))
+def logEvent(name, message):
+	print("%s %s" % (name.ljust(15), message))
+
+
+class Priority:
+	RESOURCE_EXEC_COMPLETE = 0
+	REQUEST_COMPLETE = 1
+	ADMIT_NEXT = 10
+	RESOURCE_EXEC_NEXT = 11
 
 
 class ClosedLoopWorkload:
 
-	def __init__(self, workload_id, user_id, model_id, request_generator, concurrency):
+	def __init__(self, workload_id, user_id, model_id, request_generator, concurrency, admission_control):
 		self.workload_id = workload_id
 		self.user_id = user_id
 		self.model_id = model_id
 		self.request_generator = request_generator
 		self.random = RandomState(random_seed + workload_id)
+		self.admission_control = admission_control
 		for i in range(concurrency):
 			self.start_next_request()
 
 	def start_next_request(self):
 		resource_requirements = self.request_generator.make_request(self.random)
-		request = Request(self, resource_requirements)
-		#print 'Your name is '
-		#print("%s" % (request))
+		request = Request(self, self.admission_control, resource_requirements)
 		request.begin()
 
 	def request_completed(self, request):
@@ -72,12 +80,14 @@ class Request:
 
 	request_id_seed = 0
 
-	def __init__(self, workload, resource_requirements):
+	def __init__(self, workload, admission_control, resource_requirements):
 		self.request_id = Request.request_id_seed
 		Request.request_id_seed += 1
 
 		self.workload = workload
-		self.pending_stages = [Stage(self, r, q, i) for (i, (r, q)) in enumerate(resource_requirements)]
+
+		self.admission_control = admission_control
+		self.pending_stages = [ResourceStage(self, r, q, i) for (i, (r, q)) in enumerate(resource_requirements)]
 		self.completed_stages = []
 
 		self.arrival = None
@@ -86,7 +96,12 @@ class Request:
 	def begin(self):
 		self.arrival = now()
 		logEvent("Arrive", "User %d, %s: %s" % (self.workload.user_id, self, self.verbose_description()))
-		self.execute_next_stage()
+		self.admission_control.enqueue(self)
+		#self.execute_next_stage()
+
+	def admitted(self):
+		logEvent("Admit", "User %d, %s: %s" % (self.workload.user_id, self, self.verbose_description()))
+		self.execute_next_stage();
 
 	def execute_next_stage(self):
 		self.pending_stages[0].execute()
@@ -95,6 +110,7 @@ class Request:
 		self.completed_stages.append(self.pending_stages[0])
 		self.pending_stages = self.pending_stages[1:]
 		if len(self.pending_stages) == 0:
+			self.admission_control.completed(self)
 			self.complete()
 		else:
 			self.execute_next_stage()
@@ -112,12 +128,100 @@ class Request:
 		return "Request %d" % self.request_id
 
 
-		
+class AdmissionControl:
+
+	def __init__(self):
+		pass
+
+	def enqueue(self, request):
+		pass
+
+	def completed(self, request):
+		pass
 
 
-class Stage:
+class NoAdmissionControl(AdmissionControl):
+
+	def __init__(self):
+		pass
+
+	def enqueue(self, request):
+		request.admitted()
+
+	def completed(self, request):
+		pass
+
+
+class FixedConcurrencyAdmissionControl(AdmissionControl):
+
+	def __init__(self, concurrency, queue):
+		self.concurrency = concurrency
+		self.count = 0
+		self.queue = queue
+
+	def _schedule_admission(self):
+		q.schedule(0, Priority.ADMIT_NEXT, self._admit_if_possible)
+
+	def _admit_if_possible(self):
+		while self.count < self.concurrency and not self.queue.is_empty():
+			request_to_admit = self.queue.dequeue()
+			self.count += 1
+			request_to_admit.admitted()
+
+	def enqueue(self, request):
+		self.queue.enqueue(request)
+		self._schedule_admission()
+
+	def completed(self, request):
+		self.queue.completed(request)
+		self.count -= 1
+		self._schedule_admission()
+
+
+class Queue:
+
+	def __init__(self):
+		pass
+
+	def is_empty(self):
+		return True
+
+	def enqueue(self, request):
+		pass
+
+	def dequeue(self):
+		return None
+
+	def completed(self, request):
+		pass
+
+class FIFOQueue(Queue):
+
+	def __init__(self):
+		self.pending = deque()
+
+	def is_empty(self):
+		return len(self.pending) == 0
+
+	def enqueue(self, request):
+		self.pending.append(request)
+
+	def dequeue(self):
+		return self.pending.popleft()
+
+	def completed(self, request):
+		pass
+
+
+
+class ResourceStage:
+
+	stage_id_seed = 0
 
 	def __init__(self, request, resource, quantity, stage_ix):
+		self.stage_id = ResourceStage.stage_id_seed
+		ResourceStage.stage_id_seed += 1
+
 		self.request = request
 		self.resource = resource
 		self.quantity = quantity
@@ -163,56 +267,133 @@ class RequestGenerator:
 
 
 
+class VirtualTimeConsumptionTracker:
+
+	def __init__(self, capacity):
+		self.capacity = capacity
+
+		self.t = 0
+		self.vt = 0
+
+		self.ongoing = []
+
+		self.iteration = 0
+
+	def _advance_to_now(self):
+		if len(self.ongoing) == 0:
+			self.t = now()
+			self.vt = 0
+		else:
+			cur_t = now()
+			self.vt += (cur_t - self.t) * (self.capacity / len(self.ongoing))
+			self.t = cur_t
+
+
+	def _check_for_completions(self):
+		while len(self.ongoing) > 0:
+			finish_vt, callback = self.ongoing[0]
+			if finish_vt <= self.vt:
+				self.ongoing.pop(0)
+				callback()
+			else:
+				return
+
+	def add(self, quantity, on_complete):
+		self._advance_to_now()
+		finish_vt = self.vt + quantity
+		self.ongoing.append((finish_vt, on_complete))
+		self.ongoing.sort()
+		self._schedule_next_completion()
+
+	def _check_completions_callback(self, iteration, callback_vt):
+		if self.iteration == iteration:
+			self.t = now()
+			self.vt = callback_vt
+			self._check_for_completions()
+			self._schedule_next_completion()
+
+	def _schedule_next_completion(self):
+		if len(self.ongoing) > 0:
+			self.iteration += 1
+			finish_vt, _callback = self.ongoing[0]
+			delta_t = (finish_vt - self.vt) * (len(self.ongoing) / self.capacity)
+			q.schedule(delta_t, Priority.RESOURCE_EXEC_COMPLETE, partial(self._check_completions_callback, self.iteration, finish_vt))
 
 
 class Resource:
 
-	def __init__(self, name, capacity):
+	def __init__(self, name, capacity, queue, concurrency):
 		self.name = name
-		self.capacity = capacity
-		self.pending_executions = deque()
-		self.idle = True
+		self.consumption_tracker = VirtualTimeConsumptionTracker(capacity)
+		self.queue = queue
+		self.concurrency = concurrency
+		self.count = 0
+
+
+	def _exec_next(self):
+		while self.count < self.concurrency and not self.queue.is_empty():
+			self.count += 1
+			next_execution = self.queue.dequeue()
+			next_execution.dequeue = now()
+			qtime = next_execution.dequeue - next_execution.enqueue
+			#print("%s" % (next_execution.dequeue))
+			logEvent(self.name.upper(), "Dequeue %s: %s.  Queued for %s" % (next_execution, next_execution.verbose_description(), t_str(qtime)))
+			self.consumption_tracker.add(next_execution.quantity, partial(self._on_execution_completed, next_execution))
+
+
+	def _schedule_exec_next(self):
+		if self.count < self.concurrency:
+			q.schedule(0, Priority.RESOURCE_EXEC_NEXT, self._exec_next)
+
 
 	def enqueue(self, execution):
 		execution.enqueue = now()
 		logEvent(self.name.upper(), "Enqueue   %s: %s" % (execution, execution.verbose_description()))
-		self.pending_executions.append(execution)
-		if self.idle:
-			self.idle = False
-			q.schedule(0, self.execute_next)
+		self.queue.enqueue(execution)
+		self._schedule_exec_next()
 
-	def execute_next(self):
-		next_execution = self.pending_executions.popleft()
-		next_execution.dequeue = now()
-		qtime = next_execution.dequeue - next_execution.enqueue
-		#print("%s" % (qtime))
-		logEvent(self.name.upper(), "Dequeue %s: %s.  Queued for %s" % (next_execution, next_execution.verbose_description(), t_str(qtime)))
-		execution_duration = next_execution.quantity / self.capacity
-		q.schedule(execution_duration, partial(self.on_execution_completed, next_execution))
-
-	def on_execution_completed(self, execution):
+	def _on_execution_completed(self, execution):
+		self.count -= 1
 		execution.completion = now()
 		etime = execution.completion - execution.dequeue
 		logEvent(self.name.upper(), "Completed %s: %s.  Executed for %s" % (execution, execution.verbose_description(), t_str(etime)))
+		self.queue.completed(execution)
 		execution.on_complete()
-		if len(self.pending_executions) == 0:
-			self.idle = True
-		else:
-			q.schedule(0, self.execute_next)
+		self._schedule_exec_next()
 
 	def __str__(self):
 		return self.name
 
+def concurrency_generator():
+	no_of_concurrent_requests = randint(0, 4)
+	return no_of_concurrent_requests
 
+class MultipleWorker:
 
-cpu = Resource("cpu", capacity = 1024 * 1024 * 1024 * 3)
-pcie = Resource("pcie", capacity = 12 * 1024 * 1024 * 1024)
-gpu = Resource("gpu", capacity = 1024 * 1024 * 1024 * 1024)
+	def __init__(self, worker_id, no_of_workers):
+		self.worker_id = worker_id
+		self.no_of_workers = no_of_workers
+		for i in range(no_of_workers):
+			rrr = randint(0, 4)
+			#workload1 = ClosedLoopWorkload(workload_id=1, user_id=i, model_id=1, request_generator=workload1generator, concurrency=concurrency_generator(), admission_control=admissioncontrol)
+		#rrr = randint(0, 4)
 
+class CentralController:
 
-workload1generator = RequestGenerator().binomial(cpu, 100000, 10000).binomial(pcie, 128 * 1024 * 1024, 32 * 1024 * 1024).binomial(gpu, 1024 * 1024 * 1024, 128 * 1024 * 1024)
-#workload1 = ClosedLoopWorkload(workload_id=1, user_id=1, model_id=1, request_generator=workload1generator, concurrency=3)
-workload2 = ClosedLoopWorkload(workload_id=5, user_id=3, model_id=7, request_generator=workload1generator, concurrency=3)
+	def __init__(self, ):
+		kkk = randint(0, 4)
 
-for i in range(20):
+cpu = Resource("cpu", capacity = 1000000, queue = FIFOQueue(), concurrency=100)
+pcie = Resource("pcie", capacity = 1000000, queue = FIFOQueue(), concurrency=100)
+gpu = Resource("gpu", capacity = 1000000, queue = FIFOQueue(), concurrency=100)
+
+admissionqueue = FIFOQueue()
+admissioncontrol = FixedConcurrencyAdmissionControl(1, admissionqueue)
+
+workload1generator = RequestGenerator().binomial(cpu, 1000, 100).binomial(pcie, 4000, 1000).binomial(gpu, 1000, 200)
+for i in range(10): 
+	multipleworker = MultipleWorker(worker_id=1, no_of_workers=2)
+	workload1 = ClosedLoopWorkload(workload_id=1, user_id=i, model_id=1, request_generator=workload1generator, concurrency=concurrency_generator(), admission_control=admissioncontrol)
+
+for i in range(10):
 	q.advance()()
